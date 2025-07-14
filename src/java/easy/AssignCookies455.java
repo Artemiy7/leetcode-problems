97df7b29851527b7eabc56099331b016da04674d
Arten <artemsemeniuk77@gmail.com>
2025-07-14 14:10:11 +0300

    Added leetcode 455 Assign Cookies

---
+++ src/java/easy/AssignCookies455.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class AssignCookies455 {
+    public static int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int l1 = 0;
+        int l2 = 0;
+
+        int res = 0;
+
+        for (; l1 < g.length && l2 < s.length;) {
+            if (s[l2] >= g[l1]) {
+                res++;
+                l1++;
+            }
+            l2++;
+        }
+        return res;
+    }
+}
