ec03ad553c4a01320747de5b89d18a8b23293624
Arten <artemsemeniuk77@gmail.com>
2025-07-18 12:51:23 +0300

    Added leetcode Arithmetic Slices 413

---
+++ src/java/easy/MaximumAverageSubarrayI643.java
@@ -0,0 +1,19 @@
+package easy;
+
+public class MaximumAverageSubarrayI643 {
+
+    public double findMaxAverage(int[] nums, int k) {
+        int maxSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            maxSum += nums[i];
+        }
+
+        for (int i = k; i < nums.length; i++) {
+            int tempSum = maxSum - nums[i - k] + nums[i];
+            maxSum = Math.max(maxSum, tempSum);
+        }
+
+        return (double) maxSum/k;
+    }
+}
