3ffce46607ef40aa1bdc5469bddc740b2012bc8d
Arten <artemsemeniuk77@gmail.com>
2025-07-21 20:06:56 +0300

    Added leetcode Subarray Product Less Than K 713

---
+++ src/java/medium/SubarrayProductLessThanK713.java
@@ -0,0 +1,24 @@
+package medium;
+
+public class SubarrayProductLessThanK713 {
+
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) {
+            return 0;
+        }
+        int count = 0;
+        int left = 0;
+        int product = 1;
+        for (int right = 0; right < nums.length; right++) {
+            product *= nums[right];
+            while (product >= k) {
+                product = product / nums[left++];
+                if (left == nums.length) {
+                    return count;
+                }
+            }
+            count = count + (right - left) + 1;
+        }
+        return count;
+    }
+}
