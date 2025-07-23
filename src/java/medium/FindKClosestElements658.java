0d3de5c9f053609195d91cc56d94f1599802fa58
Arten <artemsemeniuk77@gmail.com>
2025-07-23 20:38:02 +0300

    Added leetcode solution for Find K Closest Elements 658

---
+++ src/java/medium/FindKClosestElements658.java
@@ -0,0 +1,25 @@
+package medium;
+
+public class FindKClosestElements658 {
+
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left = 0;
+        int right = arr.length - 1;
+        Arrays.asList(arr);
+        for (;right - left >= k;) {
+            if (Math.abs(arr[left] - x) > Math.abs(arr[right] - x)) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = left; i <= right; i++) {
+            res.add(arr[i]);
+        }
+
+        return res;
+    }
+}
